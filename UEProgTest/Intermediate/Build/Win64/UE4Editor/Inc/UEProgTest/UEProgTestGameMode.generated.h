138cd064c06352a4561b0d76cda5f0103b3293e3
Arne Borremans <arneborr@gmail.com>
2021-06-23 10:42:36 +0200

    Initial Commit

---
+++ UEProgTest/Intermediate/Build/Win64/UE4Editor/Inc/UEProgTest/UEProgTestGameMode.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef UEPROGTEST_UEProgTestGameMode_generated_h
+#error "UEProgTestGameMode.generated.h already included, missing '#pragma once' in UEProgTestGameMode.h"
+#endif
+#define UEPROGTEST_UEProgTestGameMode_generated_h
+
+#define UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_12_SPARSE_DATA
+#define UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_12_RPC_WRAPPERS
+#define UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAUEProgTestGameMode(); \
+	friend struct Z_Construct_UClass_AUEProgTestGameMode_Statics; \
+public: \
+	DECLARE_CLASS(AUEProgTestGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/UEProgTest"), UEPROGTEST_API) \
+	DECLARE_SERIALIZER(AUEProgTestGameMode)
+
+
+#define UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAUEProgTestGameMode(); \
+	friend struct Z_Construct_UClass_AUEProgTestGameMode_Statics; \
+public: \
+	DECLARE_CLASS(AUEProgTestGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/UEProgTest"), UEPROGTEST_API) \
+	DECLARE_SERIALIZER(AUEProgTestGameMode)
+
+
+#define UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	UEPROGTEST_API AUEProgTestGameMode(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AUEProgTestGameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(UEPROGTEST_API, AUEProgTestGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AUEProgTestGameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	UEPROGTEST_API AUEProgTestGameMode(AUEProgTestGameMode&&); \
+	UEPROGTEST_API AUEProgTestGameMode(const AUEProgTestGameMode&); \
+public:
+
+
+#define UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	UEPROGTEST_API AUEProgTestGameMode(AUEProgTestGameMode&&); \
+	UEPROGTEST_API AUEProgTestGameMode(const AUEProgTestGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(UEPROGTEST_API, AUEProgTestGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AUEProgTestGameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AUEProgTestGameMode)
+
+
+#define UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_12_PRIVATE_PROPERTY_OFFSET
+#define UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_9_PROLOG
+#define UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_12_SPARSE_DATA \
+	UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_12_RPC_WRAPPERS \
+	UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_12_INCLASS \
+	UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_12_SPARSE_DATA \
+	UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_12_INCLASS_NO_PURE_DECLS \
+	UEProgTest_Source_UEProgTest_UEProgTestGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> UEPROGTEST_API UClass* StaticClass<class AUEProgTestGameMode>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID UEProgTest_Source_UEProgTest_UEProgTestGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ UEProgTest/Intermediate/Build/Win64/UEProgTestEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.28.29337
+#include "D:/Container/Programeren/UEProgTest/Intermediate/Build/Win64/UEProgTestEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
